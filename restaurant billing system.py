92da6db675b9da3d1ae96391492f09778accd77d
JAWAD HUSSAIN <jawadhussain.py@gmail.com>
2024-07-28 22:18:46 -0700

    Update and rename PROJECT 1.py to  Restaurant billing system.py

--- restaurant billing system.py
+++ restaurant billing system.py
@@ -7,22 +7,22 @@ bill_amount = float(input("Enter bill amount: "))
 # calculate tip which is 10%
 tip = bill_amount * 0.10
 
-# calculate gst which is 20%
+# calculate GST which is 20%
 gst = bill_amount * 0.20
 
-# calculate total bill amount included tip and gst
+# calculate the total bill amount including tip and GST
 total_bill = bill_amount + tip + gst
 
 # printing total bill
 print(total_bill)
 
-# taking input from user about cash which is given by customer
+# taking input from the user about cash which the customer gives
 cash = float(input("Enter the amount of cash given: "))
 
-# calculating change which is amount of cash given minus total bill amount
+# calculating change which is the amount of cash given minus the total bill amount
 change = cash - total_bill  
 
-# printing proper bill
+# Printing proper bill
 
 # printing customer name
 print(f"Customer Name: {customer_name}" )
@@ -33,13 +33,13 @@ print(f"Bill Amount: {bill_amount}")
 # printing tip amount
 print(f"Tip (10%): {tip}")
 
-# printing gst amount
+# printing GST amount
 print(f"GST (20%): {gst}")
 
 # printing total bill amount
 print(f"Total Bill: {total_bill}")
 
-# printing cash given by customer
+# printing cash is given by the customer
 print(f"Cash Given: {cash}")
 
 # printing change
